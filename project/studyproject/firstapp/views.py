831639dfe2c5e55e2af9faf0d2c7e0f0f0e6fe23
gyudon <duck9912@naver.com>
2021-08-08 02:24:09 +0900

    Day2-2

--- project/studyproject/firstapp/views.py
+++ project/studyproject/firstapp/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .models import Question
+from django.utils import  timezone
 
 def index(request) :
-    return HttpResponse("안녕하세요. 동규의 firstapp 입니다.")
+    """
+    firstapp 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list' : question_list}
+    return render(request, 'firstapp/question_list.html', context)
 
+def detail(request, question_id) :
+    """
+    firstapp 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question' : question}
+    return render(request, 'firstapp/question_detail.html', context)
+
+def answer_create(request, question_id) :
+    """
+    firstapp 답변 등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    #방법1_Answer모델을 통해 데이터 저장
+    '''
+    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    answer.save()
+    '''
+    #방법2
+    question.answer_set.create(content=request.POST.get('content'),
+                               create_date=timezone.now())
+    #등록 후 등록한 값을 포함하여 되돌아가기 (표시하는 코드는 HTML파일에서)
+    return redirect('firstapp:detail', question_id=question_id)
 # Create your views here.
