267d7cc4a2bd199fdf8d6a11452d063496dd6815
DongGyu Jung <86935274+yummygyudon@users.noreply.github.com>
2021-09-05 00:57:47 +0900

    20210905(1)_스크롤위치-<a>태그활용

--- project/studyproject/firstapp/views/answer_views.py
+++ project/studyproject/firstapp/views/answer_views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
 from django.utils import timezone
 
 from ..forms import AnswerForm
@@ -22,7 +22,8 @@ def answer_create(request, question_id) :
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
-            return redirect('firstapp:detail', question_id=question_id)
+            return redirect('{}#answer_{}'.format(
+                resolve_url('firstapp:detail', question_id=question.id),answer.id))
     else :
         form = AnswerForm()
     context = {'question' : question, 'form' : form}
@@ -56,7 +57,8 @@ def answer_modify(request, answer_id) :
             answer.author = request.user
             answer.modify_date = timezone.now() #수정 일시 저장
             answer.save()
-            return redirect('firstapp:detail', question_id = answer.question.id)
+            return redirect('{}#answer_{}'.format(
+                resolve_url('firstapp:detail', question_id=answer.question.id), answer.id))
     else:
         form = AnswerForm(instance=answer)
     context = {'answer':answer,'form': form}
