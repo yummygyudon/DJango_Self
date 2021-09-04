f4c23e4444a235a23ed37772d410d9d959143a02
DongGyu Jung <86935274+yummygyudon@users.noreply.github.com>
2021-09-05 01:20:00 +0900

    20210905(2)_MarkDown라이브러리 활용_filter(register)

--- project/studyproject/firstapp/templatetags/firstapp_filter.py
+++ project/studyproject/firstapp/templatetags/firstapp_filter.py
@@ -1,8 +1,16 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
+#페이지 작업 활용
 @register.filter
 def sub(value, arg) :
     return value - arg
+
+@register.filter
+def mark(value) : #value는 사용자의 웹페이지 입력값
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
